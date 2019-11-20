e7b311d5ee7b837f586c0b4c733d4b32e8b35d42
dikioth <elvis@dikioth.me>
2019-11-20 18:35:29 +0100

    Typo and minor cleaning

--- basicSender/basicSender.ino
+++ basicSender/basicSender.ino
@@ -1,51 +1,3 @@
-/*
- * MIT License
- * 
- * Copyright (c) 2018 Michele Biondi, Andrea Salvatori
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
-*/
-
-/*
- * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
- * Decawave DW1000Ng library for arduino.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @file BasicSender.ino
- * Use this to test simple sender/receiver functionality with two
- * DW1000Ng:: Complements the "BasicReceiver" example sketch. 
- * 
- * @todo
- *  - move strings to flash (less RAM consumption)
- *  
- */
 #include <SPI.h>
 #include <DW1000Ng.hpp>
 
@@ -72,38 +24,19 @@ device_configuration_t DEFAULT_CONFIG = {
 
 void setup()
 {
-    // DEBUG monitoring
     Serial.begin(9600);
-    // initialize the driver
     DW1000Ng::initializeNoInterrupt(PIN_SS);
-
     DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
-    //DW1000Ng::applyInterruptConfiguration(DEFAULT_INTERRUPT_CONFIG);
-
     DW1000Ng::setDeviceAddress(5);
     DW1000Ng::setNetworkId(10);
-
     DW1000Ng::setAntennaDelay(16436);
-    // attach callback for (successfully) sent messages
-    //DW1000Ng::attachSentHandler(handleSent);
-    // start a transmission
     transmit();
 }
 
-/*
-void handleSent() {
-  // status change on sent success
-  sentAck = true;
-}
-*/
-
 void transmit()
 {
-
-    // transmit some data
     String msg = Serial.readString();
     DW1000Ng::setTransmitData(msg);
-    // delay sending the message for the given amount
     DW1000Ng::startTransmit(TransmitMode::IMMEDIATE);
     delaySent = millis();
     while (!DW1000Ng::isTransmitDone())
